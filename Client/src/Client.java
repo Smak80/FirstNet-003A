0c4c54db64510540f2b4cc15ffb1f85300c62ae9
Sergey Makletsov <smaklets@kpfu.ru>
2023-03-29 14:43:37 +0300

    Первое сетевое приложение

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Clilent {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    private int port;
+    private String host;
+
+    public Client(String host, int port){
+        this.host = host;
+        this.port = (port>=0)?port:-port;
+    }
+
+    public void start(){
+        Socket s;
+        try {
+            s = new Socket(host, port);
+            var pw = new PrintWriter(s.getOutputStream());
+            pw.println("Привет!");
+            pw.flush();
+            var br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            var serverMessage = br.readLine();
+            System.out.println("Сервер ответил: "+serverMessage);
+            s.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка: "+e.getMessage());
+        }
+    }
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+
+public class Server {
+    private int port;
+    public Server(int port){
+        this.port = (port>=0)?port:-port;
+    }
+
+    public void start(){
+        ServerSocket ss;
+
+        try {
+            ss = new ServerSocket(port);
+            var s = ss.accept();
+            var br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            var clientMessage = br.readLine();
+            System.out.println("Клиент прислал: "+clientMessage);
+            var pw = new PrintWriter(s.getOutputStream());
+            pw.println("Привет в ответ!");
+            pw.flush();
+            ss.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка: "+e.getMessage());
+        }
+    }
 }
